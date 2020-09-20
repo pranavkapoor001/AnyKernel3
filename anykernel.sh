36ad2636934dd8ed98df0b3a5c0a50d6433c8759
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-09-20 18:18:26 +0530

    anykernel: Add A11 to supported versions

--- anykernel.sh
+++ anykernel.sh
@@ -15,7 +15,7 @@ device.name2=X00TD
 device.name3=X00T
 device.name4=ASUS_X00T
 device.name5=ASUS_X00TDA
-supported.versions=8.1 - 10.0
+supported.versions=8.1 - 11
 '; } # end properties
 
 # shell variables
